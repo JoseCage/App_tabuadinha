946b00c4ec759ad10ae1cf1e0f8f1d93c4cdf98d
Valdiney França <valdiney.2@hotmail.com>
2014-03-11 11:12:00 -0300

    estado interessante
    Livre dos principais bugs

--- www/js/core.js
+++ www/js/core.js
@@ -16,30 +16,136 @@ window.onload = function() {
        var buttonAumenta =  document.getElementById('buttonAumenta'),
            buttonDiminui =  document.getElementById('buttonDiminui'),
            saida  =  document.getElementById('saida'),
-           aumenta = null, time= null;
+           aumenta = 1, 
+           time= null, 
+           tipoOperacao = 'adicao',
+           buttonAdicao = document.getElementById('buttonAdicao'),
+           buttonSubtracao = document.getElementById('buttonSubtracao'),
+           buttonMultiplicacao = document.getElementById('buttonMultiplicacao'),
+           buttonDivisao = document.getElementById('buttonDivisao');
+
+           
           
 ////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////
-// Função se auto executa a cada 1 milissegundo...
+// Função (time) auto executa a cada 1 milissegundo...
 //////////////////////////////////////////////////////            
 
            time =  window.setInterval( function() {
+                   saida.innerHTML = '';
+          
+               buttonAdicao.onclick = function() {
+                   tipoOperacao = 'adicao';
+               }
 
-                 saida.innerHTML = '';
-           	     var entradaNumber =  Number ( document.getElementById('entradaNumber').value ),
-           	         cont = null, operacao;
-                     for ( cont = 0; cont <= 10; cont++ ) {
+               buttonSubtracao.onclick = function() {
+                   tipoOperacao = 'subtracao';
+               }
+
+               buttonMultiplicacao.onclick = function() {
+                   tipoOperacao = 'multiplicacao';
+               }
+
+               buttonDivisao.onclick = function() {
+                   tipoOperacao = 'divisao';
+               }
+
+/////////////////////////////////////////////////////
+
+              if( tipoOperacao === 'adicao') {
+
+                  buttonAdicao.classList.add('menuAtivo');
+                  buttonSubtracao.classList.remove('menuAtivo');
+                  buttonMultiplicacao.classList.remove('menuAtivo');
+                  buttonDivisao.classList.remove('menuAtivo');
+                  
+              }
+
+              else if ( tipoOperacao === 'subtracao' ) {
+
+                  buttonSubtracao.classList.add('menuAtivo'); 
+                  buttonAdicao.classList.remove('menuAtivo');
+                  buttonMultiplicacao.classList.remove('menuAtivo');
+                  buttonDivisao.classList.remove('menuAtivo');
+
+              }
+
+              else if ( tipoOperacao === 'multiplicacao' ) {
+
+                  buttonMultiplicacao.classList.add('menuAtivo'); 
+                  buttonAdicao.classList.remove('menuAtivo');
+                  buttonSubtracao.classList.remove('menuAtivo');
+                  buttonDivisao.classList.remove('menuAtivo');
+
+              }
+
+              else if ( tipoOperacao === 'divisao' ) {
+
+                  buttonDivisao.classList.add('menuAtivo'); 
+                  buttonMultiplicacao.classList.remove('menuAtivo'); 
+                  buttonAdicao.classList.remove('menuAtivo');
+                  buttonSubtracao.classList.remove('menuAtivo');
+                  
+              }
+
+              var entradaNumber =  Number ( document.getElementById('entradaNumber').value ),
+           	      cont = null, operacao = null;
+                     
+                     for ( cont = 1; cont <= 10; cont++ ) {
+
+                       if ( tipoOperacao === 'adicao' ) {
+
+                           operacao = entradaNumber + cont;
+                           saida.innerHTML += '<center>' + entradaNumber + ' <span class="x"> + </span> ' + cont + ' = <span>' + operacao + '</span><br></center>';
+                           
+                       }
+
+                       else if ( tipoOperacao === 'subtracao' ) {
+
+                         for ( cont = entradaNumber; cont <= ( 10 + entradaNumber ) - 1; cont++ ) {
+
+                           operacao = cont - entradaNumber;
+                           saida.innerHTML += '<center>' + cont + ' <span class="x"> - </span> ' + entradaNumber + ' = <span>' + operacao + '</span><br></center>';
+                          
+                          }
+
+                        }
+
+                       else if  ( tipoOperacao === 'multiplicacao' ) {
+                           
                            operacao = entradaNumber * cont;
-                           saida.innerHTML += '<center>' + entradaNumber + ' <span class="x"> x </span> ' + cont + ' = <span>' + operacao + '</span><br></center>';
-                  }
+                           saida.innerHTML += '<center>' + cont + ' <span class="x"> x </span> ' + entradaNumber + ' = <span>' + operacao + '</span><br></center>';
+                        
+                        }
+
+                       else if  ( tipoOperacao === 'divisao' ) {
+                          
+                         for ( cont = 1; cont <= 10; cont++ ) {
+
+                           var auxiliar = cont * entradaNumber;
+                           operacao =  Math.floor( auxiliar / entradaNumber ) ;
+
+                           if( isNaN( operacao ) ) {
+                               saida.innerHTML = '<center><small>0 &divide; 0 = 0 <br>Zero dividido por zero será sempre zero! <b>Entendeu?</b></small></center>';
+                           }
+                           else {
+                               saida.innerHTML += '<center>' + auxiliar + ' <span class="x"> &divide; </span> ' + entradaNumber + ' = <span>' + operacao + '</span><br></center>';
+                          }
+
+                        } // end for..
+
+                      } // end else if...
+
+                    } // end firts loop...
+
 
            }, 1000 );
    
 ////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////
-// Incrementa sempre o número 1 e aloca na variável (aumenta)...
+// Incrementa sempre o número 1 ao valor alocado na variável (aumenta)...
 //////////////////////////////////////////////////////////////////
 
            buttonAumenta.onclick = function() {
@@ -50,15 +156,24 @@ window.onload = function() {
            }
 
 ///////////////////////////////////////////////////////////////////
-// Decrementa sempre 1 o valor alocado na variável (aumenta)...
+// Decrementa sempre 1 do valor alocado na variável (aumenta)...
 //////////////////////////////////////////////////////////////////
 
            buttonDiminui.onclick = function() {
                
                  aumenta  = aumenta - 1;
-                 document.getElementById('entradaNumber').value = aumenta;
+
+                  if ( aumenta < 1 ) {
+
+                       aumenta = 0;
+                       document.getElementById('entradaNumber').value = aumenta;
+                  }
+                  else {
+                       document.getElementById('entradaNumber').value = aumenta;
+                 }
            }
 
 ////////////////////////////////////////////////////////////////
+
 
 } // end window...
